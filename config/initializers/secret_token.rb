28e7d88b7db159ca0ebb82ff36b10de1ea97be7c
Justin Danks <jpdanks@gmail.com>
2013-07-13 11:52:29 -0400

    Created a Rails app with status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Strive::Application.config.secret_key_base = '8609a8f4d0f688d18cff3d07802e76a3afaec5bf41915b584ce852bedb2beee465e8c3911dbe4af206d1c86329dc67b5072554f52895eed3010df4e194ba8b6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Strive::Application.config.session_store :cookie_store, key: '_strive_session'
